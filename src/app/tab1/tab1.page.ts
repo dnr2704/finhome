deadc1103a297af0a8bbea1ab6596c615de2eeb1
Darlan <dnr2704@gmail.com>
2021-09-17 13:26:56 -0300

    Layout da página inicial com slides incluídos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { stringify } from 'querystring';
 
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+
 })
 export class Tab1Page {
 
   saudacao: string;
 
+   slideOpts: any = {
+    initialSlide: 0,
+    speed: 400
+   };
+
   constructor() {}
+
 
   ngOnInit() {
     let hora = new Date().getHours();
     this.saudacao = ((hora >= 1) && (hora <= 12)) ? 'Bom dia' : ((hora >= 13) && (hora <= 18)) ? 'Boa tarde' : 'Boa noite';
   }
+
+
+
+
 
 }
